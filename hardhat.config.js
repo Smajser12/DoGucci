cb4c2af6dad242a9e43dd4bbfdba44c31f86a09b
Smajser12 <nicola.brankovic@epita.fr>
2022-08-16 18:44:00 +0200

    jew

---
+++ hardhat.config.js
@@ -0,0 +1,81 @@
+require("dotenv").config({ path: ".env" });
+
+
+module.exports = {
+  networks: {
+    hardhat: {
+    },
+    dogetest: {
+      url: 'https://rpc-testnet.dogechain.dog',
+      chainId: 568,
+      accounts: [process.env.FUJI_ACCOUNT]
+    },
+    dogechain:{
+      url: 'https://api.avax-test.network/ext/bc/C/rpc',
+      
+      accounts: [process.env.FUJI_ACCOUNT]
+
+    }
+  },
+
+  ethernal:{
+    disableSync: false, // If set to true, plugin will not sync blocks & txs
+    disableTrace: false, // If set to true, plugin won't trace transaction
+    workspace: "LocalHardHat", // Set the workspace to use, will default to the default workspace (latest one used in the dashboard). It is also possible to set it through the ETHERNAL_WORKSPACE env variable
+    
+    uploadAst: true, // If set to true, plugin will upload AST, and you'll be able to use the storage feature (longer sync time though)
+    disabled: false, // If set to true, the plugin will be disabled, nohting will be synced, ethernal.push won't do anything either
+    resetOnStart: "LocalHardHat" // Pass a workspace name to reset it automatically when restarting the node, note that if the workspace doesn't exist it won't error
+  },
+
+  solidity: {
+    compilers :[
+    {
+    version: "0.8.0",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  {
+    version: "0.8.14",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  {
+    version: "0.8.1",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  {
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+  ]
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  ethernal: {
+    email: process.env.ETHERNAL_EMAIL,
+    password: process.env.ETHERNAL_PASSWORD,
+    }
+}
